be03cec9bb6e1f8221f31e59995bd30a4b06c479
Braiden Cutforth <braiden89@gmail.com>
2021-02-17 20:53:28 -0700

    Add dotenv for configuring environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
 
 const port = 5000;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Successful PassrAPI!')
+	res.send('Successful PassrAPI!');
 });
 
 app.listen(port, () => {
-    console.log('Running Passr API.');
+	console.log('Running Passr API.');
 });
