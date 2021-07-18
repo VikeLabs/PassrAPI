3e52e610cfc4be257439ad5869790ac726d2ec97
isaiahdoyle <isaiahdoyle56@gmail.com>
2021-07-18 08:42:05 -0700

    remove duplicate code

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,10 @@ import courseRouter from './routers/course';
 import cItemRouter from './routers/courseItem';
 import semesterRouter from './routers/semester';
 import initDB from './initDB';
-import { CourseInterface as course } from './models/course';
-import { CourseItemInterface as courseItem } from './models/courseItem';
-import { SemesterInterface as semester } from './models/semester';
 
 const port = 5000;
 const app = express();
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -22,15 +20,6 @@ app.use('/user', userRouter);
 app.use('/semester', semesterRouter);
 app.use('/coure', courseRouter);
 app.use('/courseItem', cItemRouter);
-
-export const checkUserId = (
-	getDocument: (
-		key: string
-	) => Promise<course> | Promise<courseItem> | Promise<semester>
-) => async (key: string, userId: string) => {
-	const document = await getDocument(key);
-	return document.owner === userId;
-};
 
 (async () => {
 	await initDB();
