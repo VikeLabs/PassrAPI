7f3d148cb9c20e331e4d48d762add82cf36d6d11
isaiahdoyle <isaiahdoyle56@gmail.com>
2021-09-09 21:43:18 -0700

    Remove try/catch from courseItemOperations.ts

--- src/operators/courseItemOperations.ts
+++ src/operators/courseItemOperations.ts
@@ -4,37 +4,27 @@ import { checkUserId } from './index';
 
 const checkCourseItemUser = checkUserId(CourseItem.get);
 
-const ERROR_RESPONSE = 'Course item not found.';
-
 export const create = async (
 	courseItem: CourseItemInterface,
 	userID: string
 ) => {
-	try {
-		const hashKey = uuidv4();
-		courseItem.id = hashKey;
-		courseItem.owner = userID;
+	const hashKey = uuidv4();
+	courseItem.id = hashKey;
+	courseItem.owner = userID;
 
-		// logs uuid of new courseItem for testing
-		console.log(courseItem.name + ' id: ' + courseItem.id);
+	// logs uuid of new courseItem for testing
+	console.log(courseItem.name + ' id: ' + courseItem.id);
 
-		await CourseItem.create(courseItem);
-	} catch (err) {
-		console.error(ERROR_RESPONSE);
-	}
+	await CourseItem.create(courseItem);
 };
 
 // NOTE: any reference to key is referring to the hash key, or the `id` parameter in all the models.
 export const read = async (key: string, userID: string) => {
-	try {
-		const courseItem = await CourseItem.get(key);
-		if (courseItem && courseItem.owner == userID) {
-			return courseItem;
-		} else {
-			throw 'ERROR - could not read courseItem with key ' + key;
-		}
-	} catch (err) {
-		console.error(ERROR_RESPONSE);
+	const courseItem = await CourseItem.get(key);
+	if (courseItem && courseItem.owner == userID) {
+		return courseItem;
+	} else {
+		throw 'ERROR - could not read courseItem with key ' + key;
 	}
 };
 
@@ -42,29 +32,21 @@ export const update = async (
 	data: Partial<CourseItemInterface>,
 	userID: string
 ) => {
-	try {
-		if (data.id) {
-			const key = data.id;
-			const isOwner = await checkCourseItemUser(key, userID);
-			if (isOwner) {
-				await CourseItem.update(data);
-			} else {
-				throw "ERROR - userID doesn't match";
-			}
+	if (data.id) {
+		const key = data.id;
+		const isOwner = await checkCourseItemUser(key, userID);
+		if (isOwner) {
+			await CourseItem.update(data);
+		} else {
+			throw "ERROR - userID doesn't match";
 		}
-	} catch (err) {
-		console.error(ERROR_RESPONSE);
 	}
 };
 
 export const del = async (key: string, userID: string) => {
-	try {
-		const isOwner = await checkCourseItemUser(key, userID);
-		if (isOwner) {
-			await CourseItem.delete(key);
-			console.log('Deletion of document with id ' + key + ' successful.');
-		}
-	} catch (err) {
-		console.error(ERROR_RESPONSE);
+	const isOwner = await checkCourseItemUser(key, userID);
+	if (isOwner) {
+		await CourseItem.delete(key);
+		console.log('Deletion of document with id ' + key + ' successful.');
 	}
 };
