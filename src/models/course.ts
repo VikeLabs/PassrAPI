d5574107b1aac02e8de976257bd09258fe1e5fc1
Amanda de Castilho <familydecastilho@gmail.com>
2021-10-11 20:19:47 -0700

    PAS-101 semester api functions (#8)
    * [PAS-103] Added create function for courses
    
    * added semesterOperations.ts
    
    * updated semester router
    
    * updated model, ops and routers
    
    * updated routers and ops
    
    * pr review changes
    
    * add status to res
    
    * error response fix
    
    * reset initDB
    
    * error message name
    
    Co-authored-by: snorrie <Samantha.Norrie@flir.com>
    Co-authored-by: Administrator <admin@192.168.50.74>

--- src/models/course.ts
+++ src/models/course.ts
@@ -4,33 +4,30 @@ import CourseItem, { CourseItemInterface } from './courseItem';
 import FractionInterface, { fraction } from './fraction';
 
 export const courseSchema = new dynamoose.Schema(
-    {
-        id: {
-			type: String,
-			hashKey: true,
+	{
+		id: String,
+		name: String,
+		desiredGrade: [Number, fraction],
+		courseItems: {
+			type: Set,
+			schema: [CourseItem],
 		},
-        name: String,
-        desiredGrade: [Number, fraction],
-        courseItems: {
-            type: Set,
-            schema: [CourseItem],
-        },
-        owner: {
-            type: String,
-            required: true,
-        },
-    },
-    {
-        timestamps: true,
-    }
+		owner: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 export interface CourseInterface extends Document {
-    id: string;
-    name: string;
-    desiredGrade: number | FractionInterface;
-    courseItems?: Set<CourseItemInterface>;
-    owner: string;
+	id: string;
+	name: string;
+	desiredGrade: number | FractionInterface;
+	courseItems: CourseItemInterface[];
+	owner: string;
 }
 
 const Course = dynamoose.model<CourseInterface>('Course', courseSchema);
