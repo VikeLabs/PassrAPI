87f2dc366779824d2967190911d75390a5df1b05
isaiahdoyle <isaiahdoyle56@gmail.com>
2022-02-18 11:36:03 -0800

    swap PUT/POST and add add user verification in semester

--- src/operators/semesterOperations.ts
+++ src/operators/semesterOperations.ts
@@ -1,50 +1,65 @@
 import Semester, { SemesterInterface } from '../models/semester';
+import { checkUserId } from './index';
+
+const checkSemesterUser = checkUserId(Semester.get);
 
 export const create = async (semester: SemesterInterface) => {
 	try {
 		await Semester.create(semester);
 	} catch (err) {
 		console.log(err);
-		throw 'ERROR: semester not created';
+		throw new Error('ERROR: semester not created');
 	}
 };
 
 // NOTE: any reference to key is referring to the hash key, or the `id` parameter in all the models.
-export const read = async (key: string) => {
-	// add some kind of security check here?
-	const semester = await Semester.get(key);
-	if (semester) {
-		return semester;
-	} else {
-		throw 'ERROR: could not read semester';
-	}
-};
-
-export const update = async (data: Partial<SemesterInterface>) => {
-	let updated = false;
-	if (data.id) {
-		const key = data.id;
-		const semester = await Semester.get(key);
-		if (semester && (await Semester.update(data))) {
-			updated = true;
-		}
-	}
-	if (!updated) {
-		throw 'ERROR: semester not updated';
-	}
-};
-
-export const del = async (key: string) => {
+export const read = async (key: string, userID: string) => {
 	try {
 		const semester = await Semester.get(key);
-		if (semester) {
+		if (semester && semester.owner === userID) {
+			return semester;
+		} else {
+			throw new Error('ERROR: could not read semester');
+		}
+	} catch {
+		throw new Error();
+	}
+};
+
+export const update = async (
+	data: Partial<SemesterInterface>,
+	userID: string
+) => {
+	try {
+		let updated = false;
+		if (data.id) {
+			const key = data.id;
+			const isOwner = await checkSemesterUser(key, userID);
+			const semester = await Semester.get(key);
+			if (isOwner && semester && (await Semester.update(data))) {
+				updated = true;
+			}
+		}
+		if (!updated) {
+			throw new Error('ERROR: semester not updated');
+		}
+	} catch {
+		throw new Error();
+	}
+};
+
+export const del = async (key: string, userID: string) => {
+	try {
+		const isOwner = await checkSemesterUser(key, userID);
+		const semester = await Semester.get(key);
+		if (isOwner && semester) {
 			await Semester.delete(key);
 			console.log('Deletion of document with id ' + key + ' successful.');
 		} else {
-			throw 'ERROR';
+			throw new Error('UserID invalid.');
 		}
 	} catch (err) {
 		console.log(err);
-		throw 'ERROR: semester not deleted';
+		throw new Error('ERROR: semester not deleted');
 	}
 };
--- src/routers/semester.ts
+++ src/routers/semester.ts
@@ -8,18 +8,21 @@ const ERROR_RESPONSE = 'Semester not found.';
 semesterRouter.get('/:id', async (req, res) => {
 	try {
 		console.log('Get Semester');
-		const semester = await read(req.params.id);
-		const resData = {
-			...semester,
-			courses: Array.from(semester?.courses || []),
-		};
-		res.send(resData);
+		const userID = req.header('userID');
+		if (userID) {
+			const semester = await read(req.params.id, userID);
+			const resData = {
+				...semester,
+				courses: Array.from(semester?.courses || []),
+			};
+			res.send(resData);
+		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
 	}
 });
 
-semesterRouter.put('/', async (req, res) => {
+semesterRouter.post('/', async (req, res) => {
 	try {
 		await create(req.body);
 		console.log('Put Semester');
@@ -29,11 +32,14 @@ semesterRouter.put('/', async (req, res) => {
 	}
 });
 
-semesterRouter.post('/', async (req, res) => {
+semesterRouter.put('/', async (req, res) => {
 	try {
-		await update(req.body);
-		console.log('Post Semester');
-		res.send('Semester updated');
+		const userID = req.header('userID');
+		if (userID) {
+			await update(req.body, userID);
+			console.log('Post Semester');
+			res.send('Semester updated');
+		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
 	}
@@ -41,9 +47,12 @@ semesterRouter.post('/', async (req, res) => {
 
 semesterRouter.delete('/', async (req, res) => {
 	try {
-		await del(req.body.id);
-		console.log('Delete Semester');
-		res.send('Semester deleted');
+		const userID = req.header('userID');
+		if (userID) {
+			await del(req.body.id, userID);
+			console.log('Delete Semester');
+			res.send('Semester deleted');
+		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
 	}
