a93319f6995bbb4ddb7744ca729141d54a8d3f04
isaiahdoyle <isaiahdoyle56@gmail.com>
2022-04-08 11:49:34 -0700

    improve course data validation

--- src/routers/course.ts
+++ src/routers/course.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import Course from '../models/course';
 import { create, read, update, del } from '../operators/CourseOperations';
 
 const courseRouter = express.Router();
@@ -10,7 +11,6 @@ courseRouter.get('/:id', async (req, res) => {
 
 		if (id && userID) {
 			const course = await read(id, userID);
-
 			if (course) {
 				const resData = {
 					...course,
@@ -30,9 +30,16 @@ courseRouter.get('/:id', async (req, res) => {
 
 courseRouter.post('/', async (req, res) => {
 	try {
-		await create(req.body);
-		console.log('Put Course');
-		res.send('Put courseRouter: ' + req.body.name);
+		const userID = req.header('userID');
+		if (userID) {
+			const course = new Course({
+				owner: userID,
+				name: req.body.name,
+			});
+			await create(course);
+			console.log('Post Course');
+			res.send('Post courseRouter: ' + req.body.name);
+		}
 	} catch (err) {
 		res.status(404).send('Not found.');
 	}
@@ -41,11 +48,14 @@ courseRouter.post('/', async (req, res) => {
 courseRouter.put('/', async (req, res) => {
 	try {
 		const userID = req.header('userID');
-
 		if (userID) {
-			await update(req.body, userID);
-			console.log('Post Course');
-			res.send('Post courseRouter');
+			const course = new Course({
+				id: req.body.id,
+				name: req.body.name,
+			});
+			await update(course, userID);
+			console.log('Put Course');
+			res.send('Put courseRouter');
 		}
 	} catch (err) {
 		res.status(404).send('Not found.');
