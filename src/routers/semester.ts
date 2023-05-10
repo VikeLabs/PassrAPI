dbf1e7c6a455f09c339140e690c58c47c1434f68
isaiahdoyle <isaiahdoyle56@gmail.com>
2023-05-10 13:57:33 -0700

    validate semester router

--- src/routers/semester.ts
+++ src/routers/semester.ts
@@ -1,21 +1,27 @@
 import express from 'express';
-import { create, read, update, del } from '../operators/semesterOperations';
+import Ajv from 'ajv';
 import Semester from '../models/semester';
+import schema from '../types/schema.json';
 
 const semesterRouter = express.Router();
+const ajv = new Ajv({ removeAdditional: true });
 
 const ERROR_RESPONSE = 'Semester not found.';
 
 semesterRouter.get('/:id', async (req, res) => {
 	try {
+		const id = req.params.id;
 		const userID = req.header('userID');
-		if (userID) {
-			const semester = await read(req.params.id, userID);
-			const resData = {
-				...semester,
-				courses: Array.from(semester?.courses || []),
-			};
-			res.send(resData);
+		if (id && userID) {
+			/* leaving this here as a reminder to add courses array if necessary */
+			// const semester = await read(req.params.id, userID);
+			// const resData = {
+			// 	...semester,
+			// 	courses: Array.from(semester?.courses || []),
+			// };
+
+			// TODO: call db operation
+			res.status(200); // + .json(semester)
 		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
@@ -26,43 +32,52 @@ semesterRouter.post('/', async (req, res) => {
 	try {
 		const userID = req.header('userID');
 		if (userID) {
-			const semester = new Semester({
-				owner: userID,
-				name: req.body.name,
-				courseItems: [],
-			});
-			const created = await create(semester);
-			res.json(created);
-		}
-	} catch (err) {
-		res.status(404).send(ERROR_RESPONSE);
-	}
-});
-
-semesterRouter.put('/', async (req, res) => {
-	try {
-		const userID = req.header('userID');
-		if (userID) {
 			const body = req.body;
-			const semester = new Semester({
-				id: body.id,
-				owner: userID,
-				name: body.name,
-			});
-			const updated = await update(semester, userID);
-			res.json(updated);
+			const checkPost = ajv.compile(
+				schema.components.schemas.SemesterCreate
+			);
+
+			if (checkPost(body)) throw Error('body invalid');
+
+			const semester = new Semester({ owner: userID, ...body });
+
+			console.log(semester); // TODO: call db operation
+			res.status(201); // + .json(created);
 		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
 	}
 });
 
-semesterRouter.delete('/', async (req, res) => {
+semesterRouter.put('/:id', async (req, res) => {
 	try {
+		const id = req.params.id;
 		const userID = req.header('userID');
-		if (userID) {
-			await del(req.body.id, userID);
-			res.send('Semester deleted');
+		if (id && userID) {
+			const body = req.body;
+			const checkPut = ajv.compile(
+				schema.components.schemas.SemesterUpdate
+			);
+
+			if (checkPut(body)) throw Error('body invalid');
+
+			const semester = new Semester(body);
+
+			console.log(semester); // TODO: call db operation
+			res.status(200); // + .json(updated)
+		}
+	} catch (err) {
+		res.status(404).send(ERROR_RESPONSE);
+	}
+});
+
+semesterRouter.delete('/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		const userID = req.header('userID');
+		if (id && userID) {
+			// TODO: call db operation
+			res.status(200);
 		}
 	} catch (err) {
 		res.status(404).send(ERROR_RESPONSE);
